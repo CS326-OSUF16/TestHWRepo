138ccdcc222e100a218b3518a0b1213a04cac493
davefb <dave.f.baldwin@gmail.com>
2016-08-31 08:41:54 -0700

    A

--- templates/hw2/src/main/java/edu/osu/cs362/Card.java
+++ templates/hw2/src/main/java/edu/osu/cs362/Card.java
@@ -1,50 +1,42 @@
 package edu.osu.cs362;
 
-public enum Card{
+public enum Card {
 
-    curse(0),
-    estate(1),
-    duchy(2),
-    province(3),
+	CURSE(0), ESTATE(1), DUCHY(2), PROVINCE(3),
 
-    copper(4),
-    silver(5),
-    gold(6),
+	COPPER(4), SILVER(5), GOLD(6),
 
-    adventurer(7),
-    /* If no/only 1 treasure found, stop when full deck seen */
-    council_room(8),
-    feast(9), /* choice1 is supply # of card gained) */
-    gardens(10),
-    mine(11), /* choice1 is hand# of money to trash, choice2 is supply# of
-		 money to put in hand */
-    remodel(12), /* choice1 is hand# of card to remodel, choice2 is supply# */
-    smithy(13),
-    village(14),
+	ADVENTURER(7),
+	/* IF no/only 1 treasure found, stop when full deck seen */
+	COUNCIL_ROOM(8), FEAST(9), /* choice1 is supply # of card gained) */
+	GARDENS(10), MINE(11), /*
+							 * choice1 is hand# of money to trash, choice2 is
+							 * supply# of money to put in hand
+							 */
+	REMODEL(12), /* choice1 is hand# of card to remodel, choice2 is supply# */
+	SMITHY(13), VILLAGE(14),
 
-    baron(15), /* choice1: boolean for discard of estate */
-    /* Discard is always of first (lowest index) estate */
-    great_hall(16),
-    minion(17), /* choice1:  1 = +2 coin, 2 = redraw */
-    steward(18), /* choice1: 1 = +2 card, 2 = +2 coin, 3 = trash 2 (choice2,3) */
-    tribute(19),
+	BARON(15), /* choice1: boolean for discard of estate */
+	/* Discard is always of first (lowest index) estate */
+	GREAT_HALL(16), MINION(17), /* choice1: 1 = +2 coin, 2 = redraw */
+	STEWARD(18), /*
+					 * choice1: 1 = +2 card, 2 = +2 coin, 3 = trash 2 (choice2,3)
+					 */
+	TRIBUTE(19),
 
-    ambassador(20), /* choice1 = hand#, choice2 = number to return to supply */
-    cutpurse(21),
-    embargo(22), /* choice1 = supply# */
-    outpost(23),
-    salvager(24), /* choice1 = hand# to trash */
-    sea_hag(25),
-    treasure_map(26);
+	AMBASSADOR(20), /* choice1 = hand#, choice2 = number to return to supply */
+	CUTPURSE(21), EMBARGO(22), /* choice1 = supply# */
+	OUTPOST(23), SALVAGER(24), /* choice1 = hand# to trash */
+	SEA_HAG(25), TREASURE_MAP(26);
 
+	public final int id;
 
-    public final int id;
-    Card(int v){
-	this.id = v;
-    }
+	Card(int v) {
+		this.id = v;
+	}
 
-    public int getId(){
-	return id;
-    }
+	public int getId() {
+		return id;
+	}
 
 }
