fbc116dddb079ddc6b9a4da491b165cd31bc4527
dfb-osu <baldwdav@oregonstate.edu>
2016-08-31 09:26:29 -0700

    Z

--- templates/hw2/src/test/java/edu/osu/cs362/WarmupTest.java
+++ templates/hw2/src/test/java/edu/osu/cs362/WarmupTest.java
@@ -5,39 +5,35 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import static org.junit.Assert.*;
 
-public class WarmupTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public WarmupTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( WarmupTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testFindInteger()
-    {
-
-	int result = WarmUp.findInteger(new int[]{1,2,3,4,5},100);
-	assertEquals(result,-1);
-
-	for(int i=0;i<5;i++){
-	    int result2 = WarmUp.findInteger(new int[]{1,2,3,4,5},i+1);
-	    assertEquals("findInteger(new int[]{1,2,3,4,5},"+ i+")",result2,i);
+public class WarmupTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName
+	 *            name of the test case
+	 */
+	public WarmupTest(String testName) {
+		super(testName);
 	}
-    }
+
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(WarmupTest.class);
+	}
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testFindInteger() {
+
+		int result = WarmUp.findInteger(new int[] { 1, 2, 3, 4, 5 }, 100);
+		assertEquals(result, -1);
+
+		for (int i = 1; i < 5; i++) {
+			int result2 = WarmUp.findInteger(new int[] { 1, 2, 3, 4, 5 }, i + 1);
+			assertEquals("findInteger(new int[]{1,2,3,4,5}," + i + ")", result2, i);
+		}
+	}
 }
